011e2aa415f59000e6549e7f241542cf673849ad
品君林 <jinglelin82@gmail.com>
2021-01-15 23:38:35 +0900

    新增完成事項line through&灰字

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 
-const InputTodo = ({inputValue, handleChange, handleAdd}) => {
+const InputTodo = ({ inputValue, handleChange, handleAdd }) => {
   return (
     <div className="row d-flex align-items-center pb-3 border-bottom">
       <label className="col-1 d-flex align-items-center" htmlFor="input-todo">
@@ -9,8 +9,8 @@ const InputTodo = ({inputValue, handleChange, handleAdd}) => {
           add
         </span>
       </label>
-      <input type="text" id="input-todo" className="col-10 border-0" placeholder="請輸入" value={inputValue} onChange={handleChange}/>
-      <button className={clsx('col-1', 'btn', 'btn-outline-primary', 'btn-sm', 'p-0', 'd-none', {'show-add': inputValue.length > 0})} onClick={handleAdd}>新增</button>
+      <input type="text" id="input-todo" className="col-10 border-0" placeholder="請輸入" value={inputValue} onChange={handleChange} />
+      <button className={clsx('col-1', 'btn', 'btn-outline-black', 'btn-sm', 'p-0', 'd-none', { 'show-add': inputValue.length > 0 })} onClick={handleAdd}>新增</button>
     </div>
   )
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import clsx from 'clsx';
 
-const TodoItem = ({todoList, handleToggleDone}) => {
+const TodoItem = ({ todoList, handleToggleDone }) => {
   return (
     <ul className="todo-list list-unstyled">
       {todoList.map(item => {
@@ -8,10 +9,10 @@ const TodoItem = ({todoList, handleToggleDone}) => {
           <li className="list-item row py-3 d-flex align-items-center border-bottom" key={item.id}>
             <div className="col-1 d-flex align-items-center">
               <span className="material-icons" onClick={handleToggleDone(item.id)}>
-                {item.isDone? 'check_circle':'radio_button_unchecked'}
+                {item.isDone ? 'check_circle' : 'radio_button_unchecked'}
               </span>
             </div>
-            <div className="item-title col-10">
+            <div className="item-title col-10" className={clsx('item-title', 'col-10', { 'line-through': item.isDone }, { 'gray-text': item.isDone })}>
               {item.title}
             </div>
             <div className="col-1 d-flex align-items-center">
